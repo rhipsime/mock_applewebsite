7bfd9dccb8544c24768a9f23769562fb3dd9ec64
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-10-12 20:11:54 +0100

    formatted the import statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 
 const App = () => {
   return (
