a0ac81c44de2b12a6002895617ff4a3d0cf7f516
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-10-12 20:44:02 +0100

    updating the navbar with new icons and css styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,24 @@
+import { appleImg, bagImg, searchImg } from "../utils"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+        <nav className="flex w-full screen-max-width">
+          <img src={appleImg} alt="Apple" width={14} height={18} />  
+          <div>
+            {['Phones','Macbooks','Tablets'].map((nav,i)=>(
+                <div key={i}>
+                    {nav}
+                </div>))}
+                <div>
+                  <img src={searchImg} alt="search" width={18}
+                  height={18} /> 
+                   <img src={bagImg} alt="bag" width={18}
+                  height={18} /> 
+                </div>
+          </div>
+        </nav>
+    </header>
   )
 }
 
