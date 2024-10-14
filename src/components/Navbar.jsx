9d582b7a8d61ec56068f8cfd58a057b004922e9c
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-10-14 13:00:50 +0100

    importing navlists instead of using an array

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import { appleImg, bagImg, searchImg } from "../utils"
+import { navLists } from "../constants"
 
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
         <nav className="flex w-full screen-max-width">
           <img src={appleImg} alt="Apple" width={14} height={18} />  
-          <div>
+
+          <div className="flex flex-1 justify-center max-sm:hidden">
             {['Phones','Macbooks','Tablets'].map((nav,i)=>(
                 <div key={i}>
                     {nav}
