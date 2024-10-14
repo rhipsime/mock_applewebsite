3891d35addd33414b3cd90a7b86f37243839dcee
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-10-14 13:09:21 +0100

    finished styling the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,12 @@ const Navbar = () => {
           <img src={appleImg} alt="Apple" width={14} height={18} />  
 
           <div className="flex flex-1 justify-center max-sm:hidden">
-            {['Phones','Macbooks','Tablets'].map((nav,i)=>(
-                <div key={i}>
+            {navLists.map((nav,i)=>(
+                <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all" >
                     {nav}
                 </div>))}
-                <div>
+                <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+                    
                   <img src={searchImg} alt="search" width={18}
                   height={18} /> 
                    <img src={bagImg} alt="bag" width={18}
