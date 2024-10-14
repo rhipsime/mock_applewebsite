6a684a6f65d77bd5557371b5aab9c86e95e3619d
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-10-14 15:55:13 +0100

    formatting the video in the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,22 @@
 import gsap from "gsap"
 import { useGSAP } from "@gsap/react"
+import { heroVideo, smallHeroVideo } from "../utils"
 
 const Hero = () => {
-    useGSAP (() => {
-        gsap.to('#hero', {opacity: 1, delay: 1.5 })
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+    useGSAP(() => {
+      gsap.to('#hero', { opacity: 1, delay: 1.5 })
     }, [])
   return (
     <section className="w-full nav-height bg-black relative"> 
     <div className="h-5/6 w-full flex-center flex-col">
     <p id="hero" className="hero-title">iPhone 15 Pro</p>
+    <div className="md:w-10/12 w-9/12">
+    <video className="pointer-events-none" autoPlay muted playsInline={true} >
+        <source src={videoSrc} type="video/mp4" key={videoSrc} />
+    </video>
+
+    </div>
 
     </div>
 
